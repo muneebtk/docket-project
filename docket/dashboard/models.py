3d4ef61300be93ae32414d936f7d5807acd005b8
muneebtk <muhammedmuneeb3215@gmail.com>
2023-10-25 11:09:49 +0530

    final changes

--- docket/dashboard/models.py
+++ docket/dashboard/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Docket model
 class Docket(models.Model):
     name = models.CharField(max_length=100)
     start_time = models.TimeField()
